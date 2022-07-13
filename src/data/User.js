8d7d46e45270c1f554685401b7ca9610e7d62335
Jeffersono72 <jeffersonsomirandaso72@gmail.com>
2022-07-13 19:51:32 -0300

    first commit

---
+++ src/data/User.js
@@ -0,0 +1,28 @@
+export default [
+    {
+        id: 1,
+        name: 'Joseph',
+        email: 'JosephJoestar@hotmail.com',
+        avataUrl: 'https://cdn.pixabay.com/photo/2021/02/16/18/55/gamer-6022003_960_720.png',
+    },
+
+    {
+        id: 1,
+        name: 'Adrielly',
+        email: 'AdryLopes@gmail.com',
+        avataUrl: 'https://cdn.pixabay.com/photo/2021/11/12/03/04/woman-6787784_960_720.png',
+    },
+    {
+        id: 1,
+        name: 'Jefferson Lima',
+        email: 'Jeffersonoficial629@gmail.com',
+        avataUrl: 'https://cdn.pixabay.com/photo/2021/11/12/03/04/woman-6787784_960_720.png',
+    },
+
+    {
+        id: 1,
+        name: 'Luffy',
+        email: 'MonkeyDLuffy@gmail.com',
+        avataUrl: 'https://cdn.pixabay.com/photo/2022/05/16/11/01/pugs-7200102_960_720.png',
+    }
+];
